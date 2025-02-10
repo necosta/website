229a83553271a4ca17faf33e807e591b11142f3e
Nelson Costa <nelson.costa85@gmail.com>
2025-02-10 23:48:36 +0000

    Update website social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,8 +60,7 @@ const config: Config = {
     ],
   ],
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/nc_logo.png',
     blog: {
       sidebar: {
         groupByYear: false,
